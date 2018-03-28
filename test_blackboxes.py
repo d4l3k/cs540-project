5ef6a0fd7aa9e0c8045b00ce951cafcf75b4537c
Tristan Rice <rice@fn.lc>
2018-03-28 14:02:33 -0700

    TPE implementation

--- test_blackboxes.py
+++ test_blackboxes.py
@@ -2,6 +2,7 @@ import catboost
 import bayes_opt
 import numpy as np
 import scipy.optimize
+import hyperopt
 
 model = catboost.CatBoostRegressor()
 model.load_model('deathrate.cbm')
@@ -14,16 +15,33 @@ results = []
 def report(method, data):
     global results
     results += [(method, data)]
+    print_result(method, data)
+
+def print_result(method, data):
+    print('{} - {} dead over {} years'.format(method, np.sum(abs(data)), len(data)))
 
 def print_results():
     global results
     for method, data in results:
-        print('{} - {} dead over {} years'.format(method, np.sum(abs(data)), len(data)))
+        print_result(method, data)
 
 bounds = np.array([(0, 14.06), (2.23, 17.14), (0.02, 4.41), (0.13, 14.81)])
 random_state = np.random.RandomState()
 
 # TODO: give a realistic initialization point
+
+# Tree of Parzen Estimators
+x = []
+def tpe_objective(args):
+    global x
+    x += [args]
+    return model.predict([args])[0]
+
+hyperopt.fmin(tpe_objective, space=[
+    hyperopt.hp.uniform(str(i), bound[0], bound[1]) for i, bound in enumerate(bounds)
+], algo=hyperopt.tpe.suggest, max_evals=(num_years+init_years))
+
+report('Tree of Parzen Estimators', model.predict(x)[init_years:])
 
 # Bayesian Optimization
 bo = bayes_opt.BayesianOptimization(
@@ -31,12 +49,12 @@ bo = bayes_opt.BayesianOptimization(
     {'a': bounds[0], 'b': bounds[1], 'c': bounds[2], 'd': bounds[3]},
 )
 bo.maximize(init_points=init_years, n_iter=num_years)
-report('BayesianOptimization', bo.Y[init_years:])
+report('Bayesian Optimization', bo.Y[init_years:])
 
 # RandomSearch
 x_tries = random_state.uniform(bounds[:, 0], bounds[:, 1], size=(num_years, bounds.shape[0]))
 y = model.predict(x_tries)
-report('RandomSearch', y)
+report('Random Search', y)
 
 # GBDT
 def random_points(n):
