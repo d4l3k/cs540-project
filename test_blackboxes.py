b34431577f4dcf6fd62be35d1e565422f8d68364
Tristan Rice <rice@fn.lc>
2018-03-28 14:42:13 -0700

    test_blackboxes.py: report time taken + final value

--- test_blackboxes.py
+++ test_blackboxes.py
@@ -5,6 +5,7 @@ import scipy.optimize
 import hyperopt
 from sparklines import sparklines
 from tabulate import tabulate
+import time
 
 model = catboost.CatBoostRegressor()
 model.load_model('deathrate.cbm')
@@ -14,22 +15,30 @@ num_years = 20
 sample_points = 100
 
 results = []
-def report(method, data):
-    global results
-    results += [(method, data)]
-    print_result(method, data)
 
-def format_result(method, data):
-    return [method, np.sum(abs(data)), len(data), '\n'.join(sparklines(data))]
+def report(*args):
+    global results
+    results += [args]
+    print_result(*args)
+
+
+def format_result(method, data, time_taken):
+    return [method, np.sum(abs(data)), data[len(data)-1], len(data),
+            '\n'.join(sparklines(data)), time_taken]
+
 
 def print_result_table(table):
-    print(tabulate(table, headers=['Method', 'Dead per 1000', 'Years', 'Distribution']))
+    print(tabulate(table, headers=['Method', 'Dead per 1000', 'Final', 'Years',
+        'Distribution', 'Time (s)']))
+
 
 def print_result(*args):
     print_result_table([format_result(*args)])
 
+
 def print_results():
     print_result_table([format_result(*res) for res in results])
+
 
 bounds = np.array([(0, 14.06), (2.23, 17.14), (0.02, 4.41), (0.13, 14.81)])
 random_state = np.random.RandomState()
@@ -37,32 +46,40 @@ random_state = np.random.RandomState()
 # TODO: give a realistic initialization point
 
 # Tree of Parzen Estimators
+start = time.clock()
 x = []
+
+
 def tpe_objective(args):
     global x
     x += [args]
     return model.predict([args])[0]
 
+
 hyperopt.fmin(tpe_objective, space=[
-    hyperopt.hp.uniform(str(i), bound[0], bound[1]) for i, bound in enumerate(bounds)
+    hyperopt.hp.uniform(str(i), bound[0], bound[1])
+    for i, bound in enumerate(bounds)
 ], algo=hyperopt.tpe.suggest, max_evals=(num_years+init_years))
 
-report('Tree of Parzen Estimators', model.predict(x)[init_years:])
+report('Tree of Parzen Estimators', model.predict(x)[init_years:], time.clock() - start)
 
 # Bayesian Optimization
+start = time.clock()
 bo = bayes_opt.BayesianOptimization(
     lambda a, b, c, d: -model.predict([[a, b, c, d]])[0],
     {'a': bounds[0], 'b': bounds[1], 'c': bounds[2], 'd': bounds[3]},
 )
 bo.maximize(init_points=init_years, n_iter=num_years)
-report('Bayesian Optimization', bo.Y[init_years:])
+report('Bayesian Optimization', bo.Y[init_years:], time.clock() - start)
 
 # RandomSearch
+start = time.clock()
 x_tries = random_state.uniform(bounds[:, 0], bounds[:, 1], size=(num_years, bounds.shape[0]))
 y = model.predict(x_tries)
-report('Random Search', y)
+report('Random Search', y, time.clock() - start)
 
 # GBDT
+start = time.clock()
 def random_points(n):
     return random_state.uniform(bounds[:, 0], bounds[:, 1], size=(n, bounds.shape[0]))
 
@@ -83,7 +100,7 @@ for i in range(num_years):
     y_tmp = model.predict([best_x])
     y = np.append(y, y_tmp)
 
-report('GBDT', y[init_years:])
+report('GBDT', y[init_years:], time.clock() - start)
 
 print_results()
 
